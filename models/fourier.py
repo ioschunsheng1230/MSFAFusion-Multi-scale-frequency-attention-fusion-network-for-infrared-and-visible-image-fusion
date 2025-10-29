34bcdb23a43e5331ac44c9381474264885b8ee64
ioschunsheng1230 <103738551+ioschunsheng1230@users.noreply.github.com>
2025-10-29 16:31:54 +0800

    Create fourier.py

---
+++ models/fourier.py
@@ -0,0 +1,187 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from torch.fft import irfft2
+from torch.fft import rfft2
+import numbers
+from einops import rearrange
+from timm.layers.helpers import to_2tuple
+
+def rfft(x, d):
+    t = rfft2(x, dim=(-d, -1))
+    return torch.stack((t.real, t.imag), -1)
+
+
+def irfft(x, d, signal_sizes):
+    return irfft2(torch.complex(x[:, :, 0], x[:, :, 1]), s=signal_sizes, dim=(-d, -1))
+
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+
+def get_freq_indices(method):
+    assert method in ['top1','top2','top4','top8','top16','top32',
+                      'bot1','bot2','bot4','bot8','bot16','bot32',
+                      'low1','low2','low4','low8','low16','low32']
+    num_freq = int(method[3:])
+    if 'top' in method:
+        all_top_indices_x = [0,0,6,0,0,1,1,4,5,1,3,0,0,0,3,2,4,6,3,5,5,2,6,5,5,3,3,4,2,2,6,1]
+        all_top_indices_y = [0,1,0,5,2,0,2,0,0,6,0,4,6,3,5,2,6,3,3,3,5,1,1,2,4,2,1,1,3,0,5,3]
+        mapper_x = all_top_indices_x[:num_freq]
+        mapper_y = all_top_indices_y[:num_freq]
+    elif 'low' in method:
+        all_low_indices_x = [0,0,1,1,0,2,2,1,2,0,3,4,0,1,3,0,1,2,3,4,5,0,1,2,3,4,5,6,1,2,3,4]
+        all_low_indices_y = [0,1,0,1,2,0,1,2,2,3,0,0,4,3,1,5,4,3,2,1,0,6,5,4,3,2,1,0,6,5,4,3]
+        mapper_x = all_low_indices_x[:num_freq]
+        mapper_y = all_low_indices_y[:num_freq]
+    elif 'bot' in method:
+        all_bot_indices_x = [6,1,3,3,2,4,1,2,4,4,5,1,4,6,2,5,6,1,6,2,2,4,3,3,5,5,6,2,5,5,3,6]
+        all_bot_indices_y = [6,4,4,6,6,3,1,4,4,5,6,5,2,2,5,1,4,3,5,0,3,1,1,2,4,2,1,1,5,3,3,3]
+        mapper_x = all_bot_indices_x[:num_freq]
+        mapper_y = all_bot_indices_y[:num_freq]
+    else:
+        raise NotImplementedError
+    return mapper_x, mapper_y
+
+class MultiSpectralAttentionLayer(torch.nn.Module):
+    def __init__(self, channel, dct_h, dct_w, reduction = 16, freq_sel_method = 'top16'):
+        super(MultiSpectralAttentionLayer, self).__init__()
+        self.reduction = reduction
+        self.dct_h = dct_h
+        self.dct_w = dct_w
+
+        mapper_x, mapper_y = get_freq_indices(freq_sel_method)
+        self.num_split = len(mapper_x)
+        mapper_x = [temp_x * (dct_h // 7) for temp_x in mapper_x] 
+        mapper_y = [temp_y * (dct_w // 7) for temp_y in mapper_y]
+        # make the frequencies in different sizes are identical to a 7x7 frequency space
+        # eg, (2,2) in 14x14 is identical to (1,1) in 7x7
+
+        self.dct_layer = MultiSpectralDCTLayer(dct_h, dct_w, mapper_x, mapper_y, channel)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+        
+
+    def forward(self, x):
+        n,c,h,w = x.shape
+     
+        x_pooled = x
+        if h != self.dct_h or w != self.dct_w:
+            x_pooled = torch.nn.functional.adaptive_avg_pool2d(x, (self.dct_h, self.dct_w))
+            # If you have concerns about one-line-change, don't worry.   :)
+            # In the ImageNet models, this line will never be triggered. 
+            # This is for compatibility in instance segmentation and object detection.
+        y = self.dct_layer(x_pooled)
+
+        y = self.fc(y).view(n, c, 1, 1)
+        
+        return x * y.expand_as(x)
+
+
+class MultiSpectralDCTLayer(nn.Module):
+    """
+    Generate dct filters
+    """
+    def __init__(self, height, width, mapper_x, mapper_y, channel):
+        super(MultiSpectralDCTLayer, self).__init__()
+        
+        assert len(mapper_x) == len(mapper_y)
+        assert channel % len(mapper_x) == 0
+
+        self.num_freq = len(mapper_x)
+
+        # fixed DCT init
+        self.register_buffer('weight', self.get_dct_filter(height, width, mapper_x, mapper_y, channel))
+        
+        # fixed random init
+        # self.register_buffer('weight', torch.rand(channel, height, width))
+
+        # learnable DCT init
+        # self.register_parameter('weight', self.get_dct_filter(height, width, mapper_x, mapper_y, channel))
+        
+        # learnable random init
+        # self.register_parameter('weight', torch.rand(channel, height, width))
+
+        # num_freq, h, w
+
+    def forward(self, x):
+        assert len(x.shape) == 4, 'x must been 4 dimensions, but got ' + str(len(x.shape))
+        # n, c, h, w = x.shape
+
+        x = x * self.weight
+
+        result = torch.sum(x, dim=[2,3])
+        return result
+
+    def build_filter(self, pos, freq, POS):
+        result = math.cos(math.pi * freq * (pos + 0.5) / POS) / math.sqrt(POS) 
+        if freq == 0:
+            return result
+        else:
+            return result * math.sqrt(2)
+    
+    def get_dct_filter(self, tile_size_x, tile_size_y, mapper_x, mapper_y, channel):
+        dct_filter = torch.zeros(channel, tile_size_x, tile_size_y)
+
+        c_part = channel // len(mapper_x)
+
+        for i, (u_x, v_y) in enumerate(zip(mapper_x, mapper_y)):
+            for t_x in range(tile_size_x):
+                for t_y in range(tile_size_y):
+                    dct_filter[i * c_part: (i+1)*c_part, t_x, t_y] = self.build_filter(t_x, u_x, tile_size_x) * self.build_filter(t_y, v_y, tile_size_y)
+                        
+        return dct_filter
+
+class AdaFreFusion(nn.Module):
+
+    def __init__(self, in_channels, out_channels, groups=1):
+        # bn_layer not used
+        super(AdaFreFusion, self).__init__()
+
+        self.groups = groups
+        self.conv_layer = torch.nn.Conv2d(in_channels=in_channels * 2, out_channels=in_channels * 2,
+                                          kernel_size=1, stride=1, padding=0, groups=self.groups, bias=False)
+        self.bn = torch.nn.BatchNorm2d(out_channels * 2)
+        self.relu = torch.nn.ReLU(inplace=True)
+
+        self.conv_layer1 = torch.nn.Conv2d(in_channels=2, out_channels=1,
+                                           kernel_size=7, stride=1, padding=7 // 2, groups=self.groups, bias=False)
+
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        batch, c, h, w = x.size()
+        r_size = x.size()
+
+        # (batch, c, h, w/2+1, 2)
+        ffted = rfft(x, 2)
+        # (batch, c, 2, h, w/2+1)
+        ffted = ffted.permute(0, 1, 4, 2, 3).contiguous()
+        X_F = ffted.view((batch, -1,) + ffted.size()[3:])
+        X_F = self.conv_layer(X_F)  # (batch, c*2, h, w/2+1)
+        X_F = self.relu(self.bn(X_F))
+
+        avg_out = torch.mean(X_F, dim=1, keepdim=True)
+        max_out, _ = torch.max(X_F, dim=1, keepdim=True)
+        poolre = torch.cat([avg_out, max_out], dim=1)
+        ffted1 = self.conv_layer1(poolre)
+        spatial_mask = self.sigmoid(ffted1)
+
+        X_F_hat = X_F * spatial_mask
+        X_F_hat = X_F_hat.view((batch, -1, 2,) + X_F_hat.size()[2:]).permute(
+            0, 1, 3, 4, 2).contiguous()  # (batch,c, t, h, w/2+1, 2)
+
+        output = irfft(X_F_hat, 2, signal_sizes=r_size[2:])
+
+        return output
+    
+
